77148a4fbe0bcb295eda50e2bc7c1aff50e7a974
David Thureson <David.G.Thureson@gmail.com>
2020-12-09 07:28:29 -0800

    Added code to build mnemonics out of pronunciation data.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use mnembus_2000_rust::*;
+// use mnembus_2000_rust::*;
+use mnembus_2000_rust::words;
+use util_rust::log;
 
 fn main() {
     println!("Mnembus 2000 - Start");
+    log::clear();
+
     // words::survey_words();
-    //bg!(words::Word::fill_words());
-    words::survey_pronunciations();
+    let words = words::WordList::fill();
+    // words::survey_pronunciations();
+    dbg!(words::Pronunciation::fill(Some(words)).iter().take(20).collect::<Vec<_>>());
     // try_read_pronunciations();
+
+    dbg!(log::get());
     println!("Mnembus 2000 - Done");
 }
 
--- src/words.rs
+++ src/words.rs
@@ -1,8 +1,15 @@
 use crate::*;
 use util_rust::group::{Grouper, count_distinct, list_duplicates};
+use util_rust::log;
+use std::collections::BTreeMap;
 
 const WORD_FILE_NAME: &str = "English Words Top 5000.txt";
 const PRONUNCIATION_FILE_NAME: &str = "CMU Pronouncing Dictionary.txt";
+
+#[derive(Debug)]
+pub struct WordList {
+    words: BTreeMap<String, Word>,
+}
 
 #[derive(Debug)]
 pub struct Word {
@@ -13,9 +20,22 @@ pub struct Word {
     part_of_speech: String,
 }
 
-impl Word {
-    pub fn fill_words() -> Vec<Self> {
-        let mut v = vec![];
+#[derive(Debug)]
+pub struct Pronunciation {
+    word: String,
+    mnemonic: String,
+    phones: Vec<String>,
+}
+
+impl WordList {
+    pub fn new() -> Self {
+        Self {
+            words: BTreeMap::new(),
+        }
+    }
+
+    pub fn fill() -> Self {
+        let mut words = BTreeMap::new();
         let lines = util_rust::parse::read_file_as_lines(WORD_FILE_NAME)
             .iter()
             .skip(1)
@@ -28,15 +48,97 @@ impl Word {
             let part_of_speech = splits[2].trim().to_string();
             let frequency: usize = splits[3].trim().parse().unwrap();
             let dispersion: f64 = splits[4].trim().parse().unwrap();
-            v.push(Self {
+            words.insert(word.to_lowercase().to_string(), Word {
                 word,
                 rank,
                 frequency,
                 dispersion,
                 part_of_speech,
-            })
+            });
         }
+        Self {
+            words,
+        }
+    }
+
+    pub fn contains_word(&self, word: &str) -> bool {
+        self.words.contains_key(&word.to_lowercase())
+    }
+}
+
+impl Word {
+}
+
+impl Pronunciation {
+    pub fn fill(words: Option<WordList>) -> Vec<Self> {
+        //bg!(&words);
+        let mut v = vec![];
+        let lines = util_rust::parse::read_file_as_lines(PRONUNCIATION_FILE_NAME)
+            .iter()
+            .map(|line| line.trim().to_string())
+            .collect::<Vec<_>>();
+        //ebug_assert!(lines.len() >= 1000);
+        // Ignore the alternate pronunciations with a parenthetical number like:
+        //   DROP     D R AA1 P
+        //   DROP(1)  D R AO1 P
+        // These variations seem to be in vowels and weak sounds like "H", so they won't affect the
+        // mnemonics.
+        for line in lines.iter().filter(|line| !line.contains("(")) {
+            let mut splits = line
+                .split(" ")
+                .filter(|&x| !x.is_empty())
+                .map(|split| split.trim().to_string())
+                .collect::<Vec<_>>();
+            debug_assert!(splits.len() > 1);
+            let word = splits.remove(0);
+            let use_this_word = words.as_ref().map_or(true, |words| words.contains_word(&word));
+            //bg!(&word, use_this_word);
+            if use_this_word {
+                let phones = splits;
+                match Self::phones_to_mnemonic(&phones) {
+                    Ok(mnemonic) => {
+                        if mnemonic.len() > 0 {
+                            v.push(Self {
+                                word,
+                                mnemonic,
+                                phones,
+                            });
+                        }
+                    },
+                    Err(message) => {
+                        dbg!(&message);
+                        log::log(&format!("{} in {}", message, line));
+                    },
+                };
+            }
+        }
+        //bg!(v.len());
         v
+    }
+
+    fn phones_to_mnemonic(phones: &[String]) -> Result<String, String> {
+        debug_assert!(phones.len() > 0);
+        /*
+        Ok(phones
+            .iter()
+            .map(|phone| phone_to_mnemonic_number(phone).ok()?)
+            .map(|opt| opt.map_or("".to_string(), |x| x.to_string()))
+            .collect())
+         */
+        let mut mnemonic = "".to_string();
+        for phone in phones.iter() {
+            match phone_to_mnemonic_number(phone) {
+                Ok(opt) => {
+                    if let Some(n) = opt {
+                        mnemonic.push_str(&n.to_string());
+                    }
+                },
+                Err(message) => {
+                    return Err(message);
+                }
+            }
+        }
+        Ok(mnemonic)
     }
 }
 
@@ -122,3 +224,49 @@ pub fn survey_pronunciations() {
     dbg!(list_duplicates(&words));
 }
 
+fn phone_to_mnemonic_number(phone: &str) -> Result<Option<u8>, String> {
+    //bg!(&phone);
+    let phone = if phone.len() == 3 { &phone[..2] } else { phone };
+    match phone {
+        "AA" => Ok(None),
+        "AE" => Ok(None),
+        "AH" => Ok(None),
+        "AO" => Ok(None),
+        "AW" => Ok(None),
+        "AY" => Ok(None),
+        "B" => Ok(Some(9)),
+        "CH" => Ok(Some(6)),
+        "D" => Ok(Some(1)),
+        "DH" => Ok(None),
+        "EH" => Ok(None),
+        "ER" => Ok(Some(4)),
+        "EY" => Ok(None),
+        "F" => Ok(Some(8)),
+        "G" => Ok(Some(7)),
+        "HH" => Ok(None),
+        "IH" => Ok(None),
+        "IY" => Ok(None),
+        "JH" => Ok(Some(6)),
+        "K" => Ok(Some(7)),
+        "L" => Ok(Some(5)),
+        "M" => Ok(Some(3)),
+        "N" => Ok(Some(2)),
+        "NG" => Ok(Some(2)),
+        "OW" => Ok(None),
+        "OY" => Ok(None),
+        "P" => Ok(Some(9)),
+        "R" => Ok(Some(4)),
+        "S" => Ok(Some(0)),
+        "SH" => Ok(Some(6)),
+        "T" => Ok(Some(1)),
+        "TH" => Ok(Some(1)),
+        "UH" => Ok(None),
+        "UW" => Ok(None),
+        "V" => Ok(Some(8)),
+        "W" => Ok(None),
+        "Y" => Ok(None),
+        "Z" => Ok(Some(0)),
+        "ZH" => Ok(Some(6)),
+        _ => Err(format!("Unexpected phone = {}", phone)),
+    }
+}
